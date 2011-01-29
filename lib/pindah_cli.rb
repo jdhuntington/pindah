91f5bc6e534ec5191563e0338f1519ca4fc04858
JD Huntington <jd@aboutus.org>
2011-01-28 23:09:50 -0800

    Create basic structure, AndroidManifest.xml

--- lib/pindah_cli.rb
+++ lib/pindah_cli.rb
@@ -4,22 +4,58 @@ module PindahCLI
   def self.log(msg)
     STDERR.puts msg
   end
+
+  def self.mkdir(base, directory)
+    location = File.join(base, directory)
+    FileUtils.mkdir_p location
+    log "Created '#{location}'."
+    location
+  end
   
-  def self.create(namespace, location, activity_name=nil)
-    segments = namespace.split('.')
-    src_dir  = File.join(location, 'src', *segments)
-    FileUtils.mkdir_p src_dir
-    log "Created '#{src_dir}'."
-    if activity_name
-      activity_location = File.join(src_dir, "#{activity_name}.mirah")
+  def self.create(package, location, activity=nil)
+    segments = package.split('.')
+    src_dir = mkdir location, File.join('src', *segments)
+    mkdir location, 'bin'
+    mkdir location, 'libs'
+    mkdir location, 'res'
+    mkdir location, 'res/drawable-hdpi'
+    mkdir location, 'res/drawable-ldpi'
+    mkdir location, 'res/drawable-mdpi'
+    mkdir location, 'res/layout'
+    mkdir location, 'res/values'
+
+    create_android_manifest package, location, activity
+    create_initial_activity src_dir, activity if activity
+  end
+
+  def self.create_initial_activity(src_dir, activity)
+    activity_location = File.join(src_dir, "#{activity}.mirah")
+    template = File.read(File.join(File.dirname(__FILE__),
+                                   '..', 'templates',
+                                   'initial_activity.mirah'))
+
+    template.gsub!(/INITIAL_ACTIVITY/, activity)
+    File.open(activity_location, 'w') { |f| f.puts template }
+    
+    log "Created Activity '#{activity}' in '#{activity_location}'."
+  end
+
+  def self.create_android_manifest(package, location, activity)
+    manifest = File.join(location, "AndroidManifest.xml")
+
+    if activity
       template = File.read(File.join(File.dirname(__FILE__),
                                      '..', 'templates',
-                                     'initial_activity.mirah'))
-
-      File.open(activity_location, 'w') do |f|
-        f.puts template.gsub(/INITIAL_ACTIVITY/, activity_name)
-      end
-      log "Created Activity '#{activity_name}' in '#{activity_location}'."
+                                     'AndroidManifest.xml.with-activity'))
+      template.gsub!(/INITIAL_ACTIVITY/, activity) if activity
+    else
+      template = File.read(File.join(File.dirname(__FILE__),
+                                     '..', 'templates',
+                                     'AndroidManifest.xml.no-activity'))
     end
+    template.gsub!(/PACKAGE/, package)
+    File.open(manifest, 'w') { |f| f.puts template }
+    
+    log "Wrote #{manifest}."
   end
 end
--- test/pindah_cli_test.rb
+++ test/pindah_cli_test.rb
@@ -10,6 +10,7 @@ class PindahCLITest < Test::Unit::TestCase
     @project_path = File.expand_path(Tempfile.new('pindah').path + ".d")
     FileUtils.mkdir_p @project_path
     Dir.chdir @project_path
+    def PindahCLI.log(msg); end # nop
   end
 
   def teardown
@@ -18,8 +19,18 @@ class PindahCLITest < Test::Unit::TestCase
 
   def test_create_should_create_basic_project_structure
     PindahCLI.create('tld.pindah.testapp', '.')
-    assert File.directory?(File.join(@project_path, 'src', 'tld', 'pindah', 'testapp'))
+
+    directories = %w{ src/tld/pindah/testapp bin libs res
+                      res/drawable-hdpi res/drawable-ldpi
+                      res/drawable-mdpi res/layout res/values }
+
+    directories.each do |d|
+      expected = File.join(@project_path, d)
+      assert File.directory?(expected), "Expected #{expected.inspect} to be a directory."
+    end
   end
+
+
 
   def test_create_should_create_an_activity_if_desired
     PindahCLI.create('tld.pindah.testapp', '.', 'HelloWorld')
@@ -35,4 +46,36 @@ class PindahCLITest < Test::Unit::TestCase
                                    'HelloWorld.mirah'))
     assert_equal expected, actual
   end
+
+  def test_create_should_create_an_android_manifest_with_an_activity_specified
+    PindahCLI.create('tld.pindah.testapp', 'subdir', 'HelloWorld')
+
+    expected = File.read(File.join($local_pwd,
+                                   'fixtures',
+                                   'AndroidManifest.xml.with-activity'))
+    actual   = File.read(File.join(@project_path,
+                                   'subdir',
+                                   'AndroidManifest.xml'))
+    assert_equal expected, actual
+  end
+
+    def test_create_should_create_an_android_manifest_without_an_activity_specified
+    PindahCLI.create('tld.pindah.testapp', 'subdir')
+
+    expected = File.read(File.join($local_pwd,
+                                   'fixtures',
+                                   'AndroidManifest.xml.no-activity'))
+    actual   = File.read(File.join(@project_path,
+                                   'subdir',
+                                   'AndroidManifest.xml'))
+    assert_equal expected, actual
+  end
 end
+
+
+# Added file wibble/AndroidManifest.xml
+# Added file wibble/build.xml
+# Added file wibble/res/layout/main.xml
+# Added file wibble/res/values/strings.xml
+# Added file wibble/src/com/test/wibble/Wibble.java
+
